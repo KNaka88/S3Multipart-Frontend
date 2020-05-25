4a5df7c2054a8e50dbb42c3f624c379d11bed703
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-05-24 20:42:47 -0700

    More refactoring

--- src/S3Uploader/s3Uploader.ts
+++ src/S3Uploader/s3Uploader.ts
@@ -2,10 +2,9 @@ import { AxiosResponse } from "axios";
 import { S3UploaderConfig } from "./Interfaces/s3-uploader-config";
 import { S3UploadService } from "./Services/s3-upload-service";
 import { CreatePresignedUrlsRequest } from "./Interfaces/create-presigned-urls-requests";
-import { StartMultiPartUploadRequest } from "./Interfaces/start-multi-part-upload-request";
 import { CreatePresignedUrlsResponse } from "./Interfaces/create-presigned-urls-response";
 import { PutMultiPartFileresponse } from "./Interfaces/put-multipart-file-response";
-import { CompleteMultiPartUploadRequest, PartETag } from "./Interfaces/complete-multipart-upload-request";
+import { PartETag } from "./Interfaces/complete-multipart-upload-request";
 
 class S3Uploader {
     private readonly s3UploadService: S3UploadService;
@@ -21,13 +20,20 @@ class S3Uploader {
 
     public async Upload(bucketName: string, keyPrefix: string) {
         const key = keyPrefix ? `${keyPrefix}/${this.file.name}` : this.file.name;
-        const startMultiPartUploadRequest : StartMultiPartUploadRequest = {
-            bucketName,
-            key
-        };
-        const multiPartResponse = await this.s3UploadService.StartMultiPartUpload(startMultiPartUploadRequest);
-        this.uploadId = multiPartResponse.data.uploadId;        
+        const startMultiPartResponse = await this.s3UploadService.StartMultiPartUpload({ bucketName, key });
+        this.uploadId = startMultiPartResponse.data.uploadId;        
 
+        const partETags = await this.uploadFileUsingPresignedUrls(key, bucketName);
+
+        await this.s3UploadService.ComplteMultiPartUpload({
+            bucketName,
+            key,
+            partETags,
+            uploadId: this.uploadId
+        });
+    }
+
+    private async uploadFileUsingPresignedUrls(key: string, bucketName: string): Promise<Array<PartETag>> {
         const { fileChunkSize, numberOfChunks } = this.calculateChunks();
         const partETags : Array<PartETag> = [];
 
@@ -35,49 +41,18 @@ class S3Uploader {
             const res = await this.s3UploadService.CreatePresignedUrls(requests);
             const presignedUrlsResponses = res.data;
 
-            const promises = presignedUrlsResponses.map(request => this.uploadWithRetry(request, fileChunkSize, numberOfChunks));
-            const resolvedArray = await Promise.all(promises);
-            requests.partNumbers.forEach((partNumber, index) => {
+            const muiltiPartUploadRequests = presignedUrlsResponses.map(request => this.uploadWithRetry(request, fileChunkSize, numberOfChunks));
+            const mutliPartUploadResponses = await Promise.all(muiltiPartUploadRequests);
+
+            mutliPartUploadResponses.forEach((response, index) => {
                 partETags.push({
-                    eTag: resolvedArray[index].headers.etag,
-                    partNumber
-                });  
+                    eTag: response.headers.etag,
+                    partNumber: requests.partNumbers[index]
+                });
             });
         }
 
-        const completeMultiPartUploadRequest: CompleteMultiPartUploadRequest = {
-            bucketName,
-            key,
-            partETags,
-            uploadId: this.uploadId
-        };
-
-        await this.s3UploadService.ComplteMultiPartUpload(completeMultiPartUploadRequest);
-    }
-
-    private async uploadWithRetry(request: CreatePresignedUrlsResponse, fileChunkSize: number, numberOfChunks: number, iteration = 0): Promise<AxiosResponse<PutMultiPartFileresponse>> {
-        try {
-            const blob = this.createBlob(fileChunkSize, request.partNumber, numberOfChunks);
-            return await this.s3UploadService.PutMultiPartFile({ 
-                presignedUrl: request.presignedUrl.replace("https://localstack", "https://localhost"), 
-                blob,
-                headers: { 'Content-Type': this.file.type }
-            });
-        } catch (error) {
-            const errorIs400s = error.code && parseInt(error.code) >= 400 && parseInt(error.code) < 500;
-
-            if (iteration === this.numberOfRetry || errorIs400s) {
-                throw new Error("Retry Failed");
-            }
-            return this.uploadWithRetry(request, fileChunkSize, numberOfChunks, iteration++);
-        }
-    }
-
-    private createBlob(fileChunkSize: number, multiPartNumber: number, numberOfChunks: number): Blob {
-        const start = (multiPartNumber - 1) * fileChunkSize;
-        const end = multiPartNumber * fileChunkSize;
-
-        return (multiPartNumber < numberOfChunks) ? this.file.slice(start, end) : this.file.slice(start);
+        return partETags;
     }
 
     private calculateChunks() {
@@ -115,6 +90,31 @@ class S3Uploader {
                 tempCounter = 0;
             }
         }
+    }
+
+    private async uploadWithRetry(request: CreatePresignedUrlsResponse, fileChunkSize: number, numberOfChunks: number, iteration = 0): Promise<AxiosResponse<PutMultiPartFileresponse>> {
+        try {
+            const blob = this.createBlob(fileChunkSize, request.partNumber, numberOfChunks);
+            return await this.s3UploadService.PutMultiPartFile({ 
+                presignedUrl: request.presignedUrl.replace("https://localstack", "https://localhost"), 
+                blob,
+                headers: { 'Content-Type': this.file.type }
+            });
+        } catch (error) {
+            const errorIs400s = error.code && parseInt(error.code) >= 400 && parseInt(error.code) < 500;
+
+            if (iteration === this.numberOfRetry || errorIs400s) {
+                throw new Error("Retry Failed");
+            }
+            return this.uploadWithRetry(request, fileChunkSize, numberOfChunks, iteration++);
+        }
+    }
+
+    private createBlob(fileChunkSize: number, multiPartNumber: number, numberOfChunks: number): Blob {
+        const start = (multiPartNumber - 1) * fileChunkSize;
+        const end = multiPartNumber * fileChunkSize;
+
+        return (multiPartNumber < numberOfChunks) ? this.file.slice(start, end) : this.file.slice(start);
     }
 }
 
